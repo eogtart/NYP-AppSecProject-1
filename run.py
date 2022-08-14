f53b320818a2e264f074a2221a57d3eb78af803f
hoemw <hoemingwei24@gmail.com>
2022-08-14 22:09:44 +0800

    Fixed Kali SSL Cert Keys

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # app.run(host='127.0.0.1', port="5000" ,debug=True, use_reloader=False, ssl_context=('appsec+3.pem', 'appsec+3-key.pem'))
     
     # For VMWARE
-    # app.run(host='127.0.0.1', port="5000" ,debug=True, use_reloader=False, ssl_context=('127.0.0.1+2.pem', '127.0.0.1+2-key.pem'))
+    # app.run(host='127.0.0.1', port="5000" ,debug=True, use_reloader=False, ssl_context=('appsec+5.pem', 'appsec+5-key.pem'))
 
 
 
